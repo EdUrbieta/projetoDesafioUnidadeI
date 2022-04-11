3acff1c1f6c03c51b5409356f5059c5232de2ede
BonbuBot <92870959+BonbuBot@users.noreply.github.com>
2022-04-11 12:23:58 -0300

    Update desafioU1q2.py

--- desafioU1q2.py
+++ desafioU1q2.py
@@ -1,3 +1,4 @@
+#Desenvolva um algoritmo capaz de encontrar o menor dentre 3 números inteiros quaisquer dados pelo teclado.
 n1 = int(input("Digite o primeiro número:"))
 n2 = int(input("Digite o segundo número:"))
 n3 = int(input("Digite o terceiro número:"))
