389113f17d9004d4d3d7346b3dcc538fa4869875
BonbuBot <92870959+BonbuBot@users.noreply.github.com>
2022-04-11 12:20:38 -0300

    Update desafioU1q1.py

--- desafioU1q1.py
+++ desafioU1q1.py
@@ -1,3 +1,7 @@
+#Em uma loja e CD´s existem apenas quatro tipos de preços que estão associados a cores. 
+#Assim os CD´s que ficam na loja não são marcados por preços e sim por cores. 
+#Desenvolva um algoritmo que a partir da entrada da cor o software mostra o preço.
+#A loja está atualmente com a seguinte tabela de preços.
 cor = str(input("Cor do produto selecionado: "))
 if cor == "Verde":
     print("O valor do produto é 10.00 R$")
