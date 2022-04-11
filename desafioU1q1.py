6d5dc3cd72c4d371e633171fe8bd2e846a8038fb
BonbuBot <92870959+BonbuBot@users.noreply.github.com>
2022-04-11 12:07:07 -0300

    Add files via upload

---
+++ desafioU1q1.py
@@ -0,0 +1,9 @@
+cor = str(input("Cor do produto selecionado: "))
+if cor == "Verde":
+    print("O valor do produto é 10.00 R$")
+elif cor == "Azul":
+    print("O valor do produto é 20.00 R$")
+elif cor == "Amarelo":
+    print("O valor do produto é 30.00 R$")
+elif cor == "Vermelho":
+    print("O valor do produto é 40.00 R$")
---
+++ desafioU1q2.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite o primeiro número:"))
+n2 = int(input("Digite o segunda número:"))
+n3 = int(input("Digite o terceiro número:"))
+listaDeNumeros = [n1, n2, n3]
+listaDeNumeros.sort()
+print("O menor número é", listaDeNumeros[0])
